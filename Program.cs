d1b37da10f3607c3b0fa242918449b6ee501a0f7
WebmastuhB <brian@dalma.com>
2019-12-29 18:59:33 -0600

    Tightly Coupled Example

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TightCoupleLooseCoupleDIExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TightCouple Example");
+            User user = new User();
+            user.Add("This is user data");
+        }
+
+        public class User
+        {
+            MSSqlServerDatabase _database;
+
+            public User()
+            {
+                // instantiating MSSqlServerDatabase here makes the 
+                // User class tightly coupled to MSSqlServerDatabase class
+                _database = new MSSqlServerDatabase();
+            }
+            public void Add(string data)
+            {
+                _database.Persist(data);
+            }
+        }
+
+        public class MSSqlServerDatabase
+        {
+            public void Persist(string data)
+            {
+                // pretend there is a LOT of code here 
+                // doing all kinds of database stuff.
+                Console.WriteLine("MSSqlServerDatabase has persisted: " + data);
+            }
+        }
+    }
+}
